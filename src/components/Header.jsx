724e453cbeb74bc53b0b6e7a0a082643e8f3d9c6
Muhammed Mishab <mishabdotz@gmail.com>
2024-05-23 21:59:03 +0530

    Changes Update

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='sm:h-14 md:w-screen sm:w-2/6 w-screen h-20 sm:flex  bg-slate-100'>
+      <div><p className='text-red-500 text font-black text-3xl pt-6 sm:items-center ' >Even<span className='text-yellow-600'>Trio</span></p></div>
+      <div>
+        inp
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
